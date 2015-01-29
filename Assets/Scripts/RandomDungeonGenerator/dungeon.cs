54395d8e7984dd38b5220fd78d35d97a4e3947b2
barraudf <barraudf@gmail.com>
2015-01-30 00:13:20 +0100

    Implementation init_cells : done round_mask : done mask_cells : done

--- Assets/Scripts/RandomDungeonGenerator/dungeon.cs
+++ Assets/Scripts/RandomDungeonGenerator/dungeon.cs
@@ -2,29 +2,31 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+using Random = UnityEngine.Random;
+
 public class dungeon
 {
-    public Dictionary<string, int[,]> dungeon_layout = new Dictionary<string, int[,]>()
+    protected static Dictionary<string, int[][]> dungeon_layouts = new Dictionary<string, int[][]>()
     {
-        { "Box", new int[,] { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } }},
-        { "Cross", new int[,] { { 0, 1, 0 }, { 1, 1, 1 }, { 0, 1, 0 } }}
+        { "Box", new int[][] { new int[] { 1, 1, 1 }, new int[] { 1, 0, 1 }, new int[] { 1, 1, 1 } }},
+        { "Cross", new int[][] { new int[] { 0, 1, 0 }, new int[] { 1, 1, 1 }, new int[] { 0, 1, 0 } }}
     };
-    
-    public Dictionary<string, int> corridor_layout = new Dictionary<string, int>()
+
+    protected static Dictionary<string, int> corridor_layouts = new Dictionary<string, int>()
     {
         { "Labyrinth", 0 },
         { "Bent", 50 },
         { "Straight", 100 }
     };
 
-    public Dictionary<string, int> di = new Dictionary<string, int>()
+    protected static Dictionary<string, int> di = new Dictionary<string, int>()
     {
         { "north", -1 },
         { "south", 1 },
         { "west", 0 },
         { "east", 0 }
     };
-    public Dictionary<string, int> dj = new Dictionary<string, int>()
+    protected static Dictionary<string, int> dj = new Dictionary<string, int>()
     {
         { "north", 0 },
         { "south", 0 },
@@ -32,7 +34,7 @@ public class dungeon
         { "east", 1 }
     };
 
-    public Dictionary<string, string> opposite = new Dictionary<string, string>()
+    protected static Dictionary<string, string> opposite = new Dictionary<string, string>()
     {
         { "north", "south" },
         { "south", "north" },
@@ -40,70 +42,70 @@ public class dungeon
         { "east", "west" }
     };
 
-    public Dictionary<string, Dictionary<string, int[,]>> stair_end = new Dictionary<string, Dictionary<string, int[,]>>()
+    protected static Dictionary<string, Dictionary<string, int[][]>> stair_end = new Dictionary<string, Dictionary<string, int[][]>>()
     {
-        { "north", new Dictionary<string, int[,]>()
+        { "north", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}, {0,1}, {1,1} }},
-                { "corridor", new int[,] { {0,0}, {1,0}, {2,0} }},
-                { "stair", new int[,] { {0,0} }},
-                { "next", new int[,] { {1,0} }}
+                { "walled", new int[][] { new int[] {1,-1}, new int[] {0,-1}, new int[] {-1,-1}, new int[] {-1,0}, new int[] {-1,1}, new int[] {0,1}, new int[] {1,1} }},
+                { "corridor", new int[][] { new int[] {0,0}, new int[] {1,0}, new int[] {2,0} }},
+                { "stair", new int[][] { new int[] {0,0} }},
+                { "next", new int[][] { new int[] {1,0} }}
             }
         },
-        { "south", new Dictionary<string, int[,]>()
+        { "south", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {-1,-1}, {0,-1}, {1,-1}, {1,0}, {1,1}, {0,1}, {-1,1} }},
-                { "corridor", new int[,] { {0,0}, {-1,0}, {-2,0} }},
-                { "stair", new int[,] { {0,0} }},
-                { "next", new int[,] { {-1,0} }}
+                { "walled", new int[][] { new int[] {-1,-1}, new int[] {0,-1}, new int[] {1,-1}, new int[] {1,0}, new int[] {1,1}, new int[] {0,1}, new int[] {-1,1} }},
+                { "corridor", new int[][] { new int[] {0,0}, new int[] {-1,0}, new int[] {-2,0} }},
+                { "stair", new int[][] { new int[] {0,0} }},
+                { "next", new int[][] { new int[] {-1,0} }}
             }
         },
-        { "west", new Dictionary<string, int[,]>()
+        { "west", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {-1,1}, {-1,0}, {-1,-1}, {0,-1}, {1,-1}, {1,0}, {1,1} }},
-                { "corridor", new int[,] { {0,0}, {0,1}, {0,2} }},
-                { "stair", new int[,] { {0,0} }},
-                { "next", new int[,] { {0,1} }}
+                { "walled", new int[][] { new int[] {-1,1}, new int[] {-1,0}, new int[] {-1,-1}, new int[] {0,-1}, new int[] {1,-1}, new int[] {1,0}, new int[] {1,1} }},
+                { "corridor", new int[][] { new int[] {0,0}, new int[] {0,1}, new int[] {0,2} }},
+                { "stair", new int[][] { new int[] {0,0} }},
+                { "next", new int[][] { new int[] {0,1} }}
             }
         },
-        { "east", new Dictionary<string, int[,]>()
+        { "east", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {-1,-1}, {-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1} }},
-                { "corridor", new int[,] { {0,0}, {0,-1}, {0,-2} }},
-                { "stair", new int[,] { {0,0} }},
-                { "next", new int[,] { {0,-1} }}
+                { "walled", new int[][] { new int[] {-1,-1}, new int[] {-1,0}, new int[] {-1,1}, new int[] {0,1}, new int[] {1,1}, new int[] {1,0}, new int[] {1,-1} }},
+                { "corridor", new int[][] { new int[] {0,0}, new int[] {0,-1}, new int[] {0,-2} }},
+                { "stair", new int[][] { new int[] {0,0} }},
+                { "next", new int[][] { new int[] {0,-1} }}
             }
         }
     };
 
-    public Dictionary<string, Dictionary<string, int[,]>> close_end = new Dictionary<string, Dictionary<string, int[,]>>()
+    protected static Dictionary<string, Dictionary<string, int[][]>> close_end = new Dictionary<string, Dictionary<string, int[][]>>()
     {
-        { "north", new Dictionary<string, int[,]>()
+        { "north", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {0,-1}, {1,-1}, {1,0}, {1,1}, {0,1} }},
-                { "close", new int[,] { {0,0} }},
-                { "recurse", new int[,] { {-1,0} }}
+                { "walled", new int[][] { new int[] {0,-1}, new int[] {1,-1}, new int[] {1,0}, new int[] {1,1}, new int[] {0,1} }},
+                { "close", new int[][] { new int[] {0,0} }},
+                { "recurse", new int[][] { new int[] {-1,0} }}
             }
         },
-        { "south", new Dictionary<string, int[,]>()
+        { "south", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {0,-1}, {-1,-1}, {-1,0}, {-1,1}, {0,1} }},
-                { "close", new int[,] { {0,0} }},
-                { "recurse", new int[,] { {1,0} }}
+                { "walled", new int[][] { new int[] {0,-1}, new int[] {-1,-1}, new int[] {-1,0}, new int[] {-1,1}, new int[] {0,1} }},
+                { "close", new int[][] { new int[] {0,0} }},
+                { "recurse", new int[][] { new int[] {1,0} }}
             }
         },
-        { "west", new Dictionary<string, int[,]>()
+        { "west", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {-1,0}, {-1,1}, {0,1}, {1,1}, {1,0} }},
-                { "close", new int[,] { {0,0} }},
-                { "recurse", new int[,] { {0,-1} }}
+                { "walled", new int[][] { new int[] {-1,0}, new int[] {-1,1}, new int[] {0,1}, new int[] {1,1}, new int[] {1,0} }},
+                { "close", new int[][] { new int[] {0,0} }},
+                { "recurse", new int[][] { new int[] {0,-1} }}
             }
         },
-        { "east", new Dictionary<string, int[,]>()
+        { "east", new Dictionary<string, int[][]>()
             {
-                { "walled", new int[,] { {-1,0}, {-1,-1}, {0,-1}, {1,-1}, {1,0} }},
-                { "close", new int[,] { {0,0} }},
-                { "recurse", new int[,] { {0,1} }}
+                { "walled", new int[][] { new int[] {-1,0}, new int[] {-1,-1}, new int[] {0,-1}, new int[] {1,-1}, new int[] {1,0} }},
+                { "close", new int[][] { new int[] {0,0} }},
+                { "recurse", new int[][] { new int[] {0,1} }}
             }
         }
     };
@@ -138,8 +140,123 @@ public class dungeon
     protected static uint BLOCK_CORR = BLOCKED | PERIMETER | CORRIDOR;
     protected static uint BLOCK_DOOR = BLOCKED | DOORSPACE;
 
+    public int seed;
+    public int n_rows;
+    public int n_cols;
+    public string dungeon_layout;
+    public int room_min;
+    public int room_max;
+    public string room_layout;
+    public string corridor_layout;
+    public int remove_deadends;
+    public int add_stairs;
+    public string map_style;
+    public int cell_size;
+
+    public int n_i;
+    public int n_j;
+    public int max_row;
+    public int max_col;
+    public int n_rooms;
+    public int room_base;
+    public int room_radix;
+    public uint[][] cell;
+
     public dungeon()
     {
-        
+        get_opts();
+        create_dungeon();
+    }
+
+    protected void get_opts()
+    {
+        seed = System.Environment.TickCount;
+        n_rows = 39;
+        n_cols = 39;
+        dungeon_layout = "None";
+        room_min = 3;
+        room_max = 9;
+        room_layout = "Scattered";
+        corridor_layout = "Bent";
+        remove_deadends = 50;
+        add_stairs = 2;
+        map_style = "Standard";
+        cell_size = 18;
+    }
+
+    private void create_dungeon()
+    {
+        n_i = (int)n_rows / 2;
+        n_j = (int)n_cols / 2;
+        n_rows = n_i * 2;
+        n_cols = n_j * 2;
+        max_row = n_rows - 1;
+        max_col = n_cols - 1;
+        n_rooms = 0;
+
+        int max = room_max;
+        int min = room_min;
+        room_base = (int)((min + 1) / 2);
+        room_radix = (int)((max - min) / 2) + 1;
+
+        init_cells();
+        //$dungeon = &emplace_rooms($dungeon);
+        //$dungeon = &open_rooms($dungeon);
+        //$dungeon = &label_rooms($dungeon);
+        //$dungeon = &corridors($dungeon);
+        //$dungeon = &emplace_stairs($dungeon) if ($dungeon->{'add_stairs'});
+        //$dungeon = &clean_dungeon($dungeon);
+    }
+
+    private void init_cells()
+    {
+        for(int r = 0; r <= n_rows; r++)
+        {
+            for(int c = 0; c <= n_cols; c++)
+            {
+                cell[r][c] = NOTHING;
+            }
+        }
+        Random.seed = seed;
+
+        if (dungeon_layouts.ContainsKey(dungeon_layout))
+        {
+            mask_cells(dungeon_layouts[dungeon_layout]);
+        }
+        else if (dungeon_layout == "Round")
+        {
+            round_mask();
+        }
+    }
+
+    private void round_mask()
+    {
+        int center_r = (int)n_rows / 2;
+        int center_c = (int)n_cols / 2;
+
+        for (int r = 0; r <= n_rows; r++)
+        {
+            for (int c = 0; c <= n_cols; c++)
+            {
+                float d = Mathf.Sqrt( Mathf.Pow(r - center_r, 2) + Mathf.Pow(c - center_c, 2));
+                if (d > center_c)
+                    cell[r][c] = BLOCKED;
+            }
+        }
+    }
+
+    private void mask_cells(int[][] p)
+    {
+        int r_x = (p.Length / (n_rows + 1));
+        int c_x = (p[0].Length / (n_cols + 1));
+
+        for (int r = 0; r <= n_rows; r++)
+        {
+            for (int c = 0; c <= n_cols; c++)
+            {
+                if(p[r * r_x][c * c_x] == 1)
+                cell[r][c] = BLOCKED;
+            }
+        }
     }
 }
