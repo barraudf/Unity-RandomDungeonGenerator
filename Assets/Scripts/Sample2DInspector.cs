db2f111c7e63ced38aa573887f70694c6c46195b
barraudf <barraudf@gmail.com>
2015-02-13 13:43:28 +0100

    Added : Export to PNG button in the inspector This will save the generated dungeon texture from the Sample2DRenderer to a file.

--- Assets/Scripts/Sample2DInspector.cs
+++ Assets/Scripts/Sample2DInspector.cs
@@ -19,5 +19,29 @@ public class Sample2DInspector : Editor
         {
             r.Seed = System.Environment.TickCount;
         }
+        if(GUILayout.Button("Export To PNG"))
+        {
+            string path = EditorUtility.SaveFilePanel(
+                    "Save texture as PNG",
+                    "",
+                    "Dungeon.png",
+                    "png");
+
+            if (path.Length != 0)
+            {
+                Texture2D tex = r.MeshRenderer.sharedMaterials[0].mainTexture as Texture2D;
+
+                // Convert the texture to a format compatible with EncodeToPNG
+                if (tex.format != TextureFormat.ARGB32 && tex.format != TextureFormat.RGB24)
+                {
+                    Texture2D newTexture = new Texture2D(tex.width, tex.height);
+                    newTexture.SetPixels(tex.GetPixels(0), 0);
+                    tex = newTexture;
+                }
+                byte[] pngData = tex.EncodeToPNG();
+                if (pngData != null)
+                    System.IO.File.WriteAllBytes(path, pngData);
+            }
+        }
     }
 }
